9e0f14650901c0fe18ec42305bfac14751ce857c
YannisMarketakis <ymark_em@yahoo.gr>
2016-12-29 13:43:00 +0200

    proper implementation of ImporterException

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/exceptions/ImporterException.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/exceptions/ImporterException.java
@@ -3,16 +3,16 @@ package gr.forth.ics.isl.triplestoremethods.exceptions;
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
-public class TripleStoreConnectionException extends Exception{
-    public TripleStoreConnectionException(){
-        super("An error occurred while connection to the triplestore");
+public class ImporterException extends Exception{
+    public ImporterException(){
+        super("An error occurred while importing data to the triplestore");
     }
     
-    public TripleStoreConnectionException(String msg){
+    public ImporterException(String msg){
         super(msg);
     }
     
-    public TripleStoreConnectionException(String msg, Throwable thr){
+    public ImporterException(String msg, Throwable thr){
         super(msg,thr);
     }
 }
