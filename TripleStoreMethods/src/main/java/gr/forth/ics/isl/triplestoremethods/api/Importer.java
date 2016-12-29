5e9d7cbc2d72d5249cbc3435e0ab2be9c7e9f722
YannisMarketakis <ymark_em@yahoo.gr>
2016-12-29 13:44:49 +0200

    throw exceptions for all types of import methods

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Importer.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Importer.java
@@ -10,11 +10,11 @@ import java.io.File;
 public interface Importer {
     public void importResource(File file, String graphspace, RdfFormat format) throws ImporterException;
     
-    public void importResource(String content, String graphspace, RdfFormat format);
+    public void importResource(String content, String graphspace, RdfFormat format) throws ImporterException;
     
-    public void importResources(String graphspace, RdfFormat format, File ... files);
+    public void importResources(String graphspace, RdfFormat format, File ... files) throws ImporterException;
     
-    public void importResources(String graphspace, RdfFormat format, String ... files);
+    public void importResources(String graphspace, RdfFormat format, String ... files) throws ImporterException;
     
-    public void importResourcesFromFolder(File folder, String graphspace, boolean recursiveImport, RdfFormat format);
+    public void importResourcesFromFolder(File folder, String graphspace, boolean recursiveImport, RdfFormat format) throws ImporterException;
 }
