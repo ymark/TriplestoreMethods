b5c1b1f8dc52ca55f5d1565f72d4ab484d3920cd
YannisMarketakis <ymark_em@yahoo.gr>
2016-12-29 13:43:31 +0200

    factory method for Virtuoso

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/TripleStoreFactory.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/TripleStoreFactory.java
@@ -1,8 +1,14 @@
 package gr.forth.ics.isl.triplestoremethods;
 
+import gr.forth.ics.isl.triplestoremethods.api.TripleStoreConnector;
+import gr.forth.ics.isl.triplestoremethods.exceptions.TripleStoreConnectionException;
+import gr.forth.ics.isl.triplestoremethods.impl.virtuoso.VirtuosoConnector;
+
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public class TripleStoreFactory {
-
+    public static TripleStoreConnector connectToVirtuoso(String host, int port, String username, String password) throws TripleStoreConnectionException{
+        return new VirtuosoConnector(host, port, username, password);
+    }
 }
