8021a8c80e5a1eaf14702c042dac9e58e31a5009
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-09 16:34:57 +0200

    fixed the type of a parameter

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
@@ -1,13 +1,13 @@
 package gr.forth.ics.isl.triplestoremethods.api;
 
+import gr.forth.ics.isl.triplestoremethods.common.RdfFormat;
 import gr.forth.ics.isl.triplestoremethods.exceptions.ExporterException;
-import org.openrdf.rio.RDFFormat;
 
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public interface Exporter {
-    public String exportToString(String graphspace, RDFFormat format) throws ExporterException;
+    public String exportToString(String graphspace, RdfFormat format) throws ExporterException;
     
-    public String exportToFile(String graphspace, RDFFormat format) throws ExporterException;
+    public String exportToFile(String graphspace, RdfFormat format) throws ExporterException;
 }
