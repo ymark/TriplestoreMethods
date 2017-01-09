0497db9865d8f371f271c4b5c20032c88d7d9d95
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-09 16:44:05 +0200

    changed the signatures (they now make more sense)

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
@@ -2,6 +2,7 @@ package gr.forth.ics.isl.triplestoremethods.api;
 
 import gr.forth.ics.isl.triplestoremethods.common.RdfFormat;
 import gr.forth.ics.isl.triplestoremethods.exceptions.ExporterException;
+import java.io.File;
 
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
@@ -9,5 +10,5 @@ import gr.forth.ics.isl.triplestoremethods.exceptions.ExporterException;
 public interface Exporter {
     public String exportToString(String graphspace, RdfFormat format) throws ExporterException;
     
-    public String exportToFile(String graphspace, RdfFormat format) throws ExporterException;
+    public void exportToFile(File file, String graphspace, RdfFormat format) throws ExporterException;
 }
