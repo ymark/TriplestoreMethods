8bb9d543b2f1feed12a828bc0f6685f8a6432063
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-09 17:01:27 +0200

    removed unwanted imports

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/impl/virtuoso/VirtuosoImporter.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/impl/virtuoso/VirtuosoImporter.java
@@ -7,8 +7,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import org.apache.log4j.Logger;
 import org.openrdf.model.URI;
