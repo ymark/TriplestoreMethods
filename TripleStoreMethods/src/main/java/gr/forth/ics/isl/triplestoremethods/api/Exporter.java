40dc8c77dbe7f0bf56cabcc7c1712971e5868823
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-09 16:21:25 +0200

    signatures for the exporter functions

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
@@ -1,8 +1,12 @@
 package gr.forth.ics.isl.triplestoremethods.api;
 
+import org.openrdf.rio.RDFFormat;
+
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public interface Exporter {
-
+    public String exportToString(String graphspace, RDFFormat format);
+    
+    public String exportToFile(String graphspace, RDFFormat format);
 }
