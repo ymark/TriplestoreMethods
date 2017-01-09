67f529dc10a1b320070b7cb98bca946e071a7ee0
YannisMarketakis <ymark_em@yahoo.gr>
2017-01-09 16:32:25 +0200

    throw exporter exception for the corresponding methods

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Exporter.java
@@ -1,12 +1,13 @@
 package gr.forth.ics.isl.triplestoremethods.api;
 
+import gr.forth.ics.isl.triplestoremethods.exceptions.ExporterException;
 import org.openrdf.rio.RDFFormat;
 
 /**
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public interface Exporter {
-    public String exportToString(String graphspace, RDFFormat format);
+    public String exportToString(String graphspace, RDFFormat format) throws ExporterException;
     
-    public String exportToFile(String graphspace, RDFFormat format);
+    public String exportToFile(String graphspace, RDFFormat format) throws ExporterException;
 }
