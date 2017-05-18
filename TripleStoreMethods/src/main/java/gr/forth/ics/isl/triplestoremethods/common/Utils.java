564d0466f54232d44d642448470fbfa420c6c914
YannisMarketakis <ymark_em@yahoo.gr>
2017-05-18 11:16:04 +0300

    added javadocs comments

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/common/Utils.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/common/Utils.java
@@ -10,6 +10,12 @@ import org.apache.log4j.Logger;
 public class Utils {
     private static final Logger logger=Logger.getLogger(Utils.class);
     
+    /** Check if the given string forms a valid URI.
+     * Valid URIs are considered the valid URLs (e.g. http://example.com, or https://example.com)
+     * as well as resources following the URN scheme (e.g. urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6)
+     * 
+     * @param uri the string that will be checked if it forms a valid URI or not
+     * @return true if the given string is a valid URI, otherwise false */
     public static boolean isValidURI(String uri){
         if(uri.toLowerCase().startsWith(Common.URN_PREFIX)){
             return true;
