d2c813fc96abdc036c42d0276aed59e47c8b6e37
YannisMarketakis <ymark_em@yahoo.gr>
2017-05-18 10:46:51 +0300

    added generics to Triple objects

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Query.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/api/Query.java
@@ -9,15 +9,15 @@ import org.apache.commons.lang3.tuple.Triple;
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public interface Query {
-    public Collection<Triple> getTriplesWith(String subject, String predicate, String object, String... graphspaces) throws QueryException;
+    public Collection<Triple<String,String,String>> getTriplesWith(String subject, String predicate, String object, String... graphspaces) throws QueryException;
     
-    public Collection<Triple> getTriplesHavingSubject(String subject, String... graphspaces) throws QueryException;
+    public Collection<Triple<String,String,String>> getTriplesHavingSubject(String subject, String... graphspaces) throws QueryException;
     
-    public Collection<Triple> getTriplesHavingPredicate(String predicate, String... graphspaces) throws QueryException;
+    public Collection<Triple<String,String,String>> getTriplesHavingPredicate(String predicate, String... graphspaces) throws QueryException;
     
-    public Collection<Triple> getTriplesHavingObject(String object, String... graphspaces) throws QueryException;
+    public Collection<Triple<String,String,String>> getTriplesHavingObject(String object, String... graphspaces) throws QueryException;
     
-    public Collection<Triple> getTriplesHavingLiteralObject(String literalObject, String... graphspaces) throws QueryException;
+    public Collection<Triple<String,String,String>> getTriplesHavingLiteralObject(String literalObject, String... graphspaces) throws QueryException;
     
     public boolean hasTriplesWith(String subject, String predicate, String object, String... graphspaces) throws QueryException;
     
