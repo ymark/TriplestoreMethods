5b248d3d25c232d1a73055ab3fd37d2b5ec45bfe
YannisMarketakis <ymark_em@yahoo.gr>
2017-05-29 14:27:12 +0300

    initial implementation of BlazegraphImporter

--- TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/TripleStoreFactory.java
+++ TripleStoreMethods/src/main/java/gr/forth/ics/isl/triplestoremethods/TripleStoreFactory.java
@@ -2,6 +2,7 @@ package gr.forth.ics.isl.triplestoremethods;
 
 import gr.forth.ics.isl.triplestoremethods.api.TripleStoreConnector;
 import gr.forth.ics.isl.triplestoremethods.exceptions.TripleStoreConnectionException;
+import gr.forth.ics.isl.triplestoremethods.impl.blazegraph.BlazegraphConnector;
 import gr.forth.ics.isl.triplestoremethods.impl.virtuoso.VirtuosoConnector;
 
 /**
@@ -10,5 +11,9 @@ import gr.forth.ics.isl.triplestoremethods.impl.virtuoso.VirtuosoConnector;
 public class TripleStoreFactory {
     public static TripleStoreConnector connectToVirtuoso(String host, int port, String username, String password) throws TripleStoreConnectionException{
         return new VirtuosoConnector(host, port, username, password);
+    }
+    
+    public static TripleStoreConnector connectToBlazegraph(String host) throws TripleStoreConnectionException{
+        return new BlazegraphConnector(host);
     }
 }
